6e27f29057eaccda598524532db3dd46bd166ee0
jstack123 <league@WTS5.attlocal.net>
2017-08-08 11:51:22 -0700

    8/8 day 2 code workshop
    day 2 of code workshop, 8/8/17

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -1,12 +1,13 @@
 package day1.robot;
 
 import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
-	
+
 	// 2. Create a new Robot
-	
+	Robot data = new Robot("mini");
 
 	void go() {
 		// 4. Make the robot move as fast as possible
@@ -15,20 +16,26 @@ public class FourSquare {
 
 		// 6. Do steps #7 to #8 four times...
 
-			// 7. Set the pen color to random
-	
-			// 1. Call the drawSquare() method
-	
-			// 8. Turn the robot 90 degrees to the right
+		// 7. Set the pen color to random
+
+		// 1. Call the drawSquare() method
+		drawSquare();
+
+		// 8. Turn the robot 90 degrees to the right
 
 	}
 
 	/* 3. Fill in the code to draw a square inside the method below. */
+
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		data.setSpeed(10);
+		data.penDown();
+		for (int i = 0; i < 4; i++) {
+			data.move(100);
+			data.turn(90);
+		}
 
-		
-		
 	}
 
 	public static void main(String[] args) {
--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,15 +1,17 @@
 package day1.robot;
 
-import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
-		
-		// START HERE
 
-	
-		
-		
+		// START HERE
+		Robot c3PO = new Robot("mini");
+		c3PO.sparkle();
+		c3PO.setSpeed(10);
+		c3PO.moveTo(200, 200);
+		c3PO.turn(90);
+		c3PO.move(100);
+
 	}
 }
